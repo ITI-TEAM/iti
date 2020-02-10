9dfbdd4bfc8b3097910fc3b325945c0046e0884e
Eissa Abdellah <eissa2020482@gmail.com>
2020-02-10 12:19:10 +0200

    login authetacion NodeJs

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,20 +1,133 @@
 const express= require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs');
-var jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 // user Model 
-const user = require('../models/user');
+const User = require('../models/user');
 
-router.get('/',(req,res,next)=>{
+// REGISTER NEW USER
+router.post("/signup",(req,res,next)=>{
 
-   
+    User.find({email:req.body.email})
+        .exec()
+        .then(user=>{
+            if(user.length >= 1){
+                return res.status(409).json({
+                    message:'email already exist'
+                });
+            }
+            else{
+                bcrypt.hash(req.body.password,10,(err,hash)=>{
+                    if(err){
+                        return res.status(500).json({
+                            error:err
+                        });
+                    } 
+                    else{
+                        const user=new User({
+                            email:req.body.email,
+                            password:hash,
+                            type:req.body.type
+                        });
+                        user.save()
+                        .then(result=>{
+                            console.log(result);
+                            return res.status(201).json({
+                                message:"user created"
+                            })
+                        })
+                        .catch(err=>{
+                            console.log(err);
+                            res.status(500).json({
+                                error:err
+                            });
+                            
+                        });
+                    }
+                });
 
-    res.status(200).json({
-        message : 'Hi from NodeJs'
+            }
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json({
+                error:err
+            });
+            
+        });
+});
+
+
+
+
+//LOGIN USER
+
+router.post("/login",(req,res,next)=>{
+    User.find({email:req.body.email})
+    .exec()
+    .then(user=>{
+        if(user.length < 1 ){
+            return res.status(401).json({
+                message:"Auth failed"
+            });
+        }
+        bcrypt.compare(req.body.password,user[0].password,(err,result)=>{
+            if(err){
+                return res.status(401).json({
+                    message:"Auth failed"
+                });
+            }
+
+            if(result){
+             const token= jwt.sign({
+                    email:user[0].email,
+                    userID:user[0]._id
+                },
+                'secret',{expiresIn:'5h'}
+                )
+                return res.status(200).json({
+                    message:"Auth succesful",
+                    token:token,
+                    type:user[0].type
+                });
+            }
+            res.status(401).json({
+                message:"Auth failed"
+            });
+                
+            
+        });
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            error:err
+        });
+        
     });
-        res.end();
 
 });
 
+
+
+
+//DELETE USER
+
+router.delete("/delete/:userID",(req,res,next)=>{
+    User.remove({_id:req.params.userID})
+            .exec()
+            .then(result=>{
+                res.status(200).json({
+                    message:"user deleted"
+                });
+            })
+            .catch(err=>{
+                console.log(err);
+                res.status(500).json({
+                    error:err
+                });
+                
+            });
+});
 module.exports = router;
