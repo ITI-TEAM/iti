b3dc6a0df24d7fd5cdcc3b4851d9ab80d1b659cb
DELL <ayag2519@gmail.com>
2020-02-11 16:05:12 +0200

    homee

--- src/app/components/home/home/home.component.ts
+++ src/app/components/home/home/home.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  posts =[
+    {  companey: "modern",name:"aaaaaaaaaaaaaaaaaaaa", photourl:'../../../../assets/images/heba.jpg'},
+    {  companey: "iti",name:"bbbbbbbbbbbbbbbbbbbb", photourl:'../../../../assets/images/heba.jpg'},
 
+    {  companey: "www",name:'zzzzzzzzzzzzzzzzzz', photourl:'../../../../assets/images/heba.jpg'},
+    ]
+    envents=[
+      {titel:'EVENT',obj:'ssssss'},
+      {titel:'eeeeeeeeee',obj:'yyyyyyy'},
+      {titel:'eeeeeeeeee',obj:'yyyyyyy'},{titel:'eeeeeeeeee',obj:'yyyyyyy'}
+    ]
+  
   constructor() { }
 
   ngOnInit() {
   }
+
 
 }
