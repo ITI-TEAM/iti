a3af7025adeb3905a58464d38b6e7675fc5ff775
Eissa Abdellah <eissa2020482@gmail.com>
2020-02-12 10:25:32 +0200

    update delete user at nodeJs

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ app.use(function(req, res, next) {
   // ROUTES
   app.use('/users',userRoutes);
   app.use('/sprofile',sprofileRoutes);
-  app.use('/sprofile',cprofileRoutes);
+  app.use('/cprofile',cprofileRoutes);
 
 
 
