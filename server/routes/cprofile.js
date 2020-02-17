79794c2cb6fd61b0d440f0e1393a9f4b6f8c1168
DELL <ayag2519@gmail.com>
2020-02-17 15:10:33 +0200

    company

--- server/routes/cprofile.js
+++ server/routes/cprofile.js
@@ -2,6 +2,10 @@ const express= require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const mongo =require ('mongodb').MongoClient;
+const objectId =require ('mongodb').ObjectID;
+
+
 
 //  Model 
 const User = require('../models/user');
@@ -11,7 +15,7 @@ const Cprofile=require('../models/cprofile');
 
 router.get("/:userID",(req,res,next)=>{
 
-    Cprofile.find({ID:req.params.userID})
+    Cprofile.findOne({ID:req.params.userID})
             .then(data=>{
                 res.status(200).json(data);
             })
@@ -22,6 +26,31 @@ router.get("/:userID",(req,res,next)=>{
                 });
                 
             });
+});
+
+router.put('/edit/:userID',function(req,res,next){
+    var item={
+        name:req.body.name,
+        telephonenumber:req.body.telephonenumber,
+        careerobjrctive:req.bady.careerobjrctive,
+        content:req.body.content,
+        email:req.bady.email,
+        address:req.bady.address,
+        datecreated:req.body.datecreated,
+        field:req.body.field,
+        description:req.body.description
+    }
+    Cprofile.findOneAndUpdate({ID:req.params.userID}, {$set:item}, {new: true})
+    .then(data=>{
+        res.status(200).json(data);
+    })
+    .catch(err=>{
+        console.log(err);
+        res.status(500).json({
+            error:err
+        });
+        
+    });
 });
 
 
