498da522d9cd8653c9687f25cabe7e2144505422
unknown <amlfakhry13@gmail.com>
2020-02-25 17:01:50 +0200

    post job design

--- src/app/components/navbar-company/navbar-company.component.ts
+++ src/app/components/navbar-company/navbar-company.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-
+declare var $: any;
 @Component({
   selector: 'app-navbar-company',
   templateUrl: './navbar-company.component.html',
@@ -13,8 +13,47 @@ export class NavbarCompanyComponent implements OnInit {
   ngOnInit() {
     this.route.paramMap.subscribe((params:ParamMap)=>{
       this.ID=params.get('ID');
-    }); 
+    });
+    
+    
+    //jquery
+$(window).scroll(function(){
+  var $w = $(this),
+      st = $w.scrollTop(),
+      navbar = $('.ftco_navbar'),
+      sd = $('.js-scroll-wrap');
+
+  if (st > 150) {
+    if ( !navbar.hasClass('scrolled') ) {
+      navbar.addClass('scrolled');	
+    }
+  } 
+  if (st < 150) {
+    if ( navbar.hasClass('scrolled') ) {
+      navbar.removeClass('scrolled sleep');
+    }
+  } 
+  if ( st > 350 ) {
+    if ( !navbar.hasClass('awake') ) {
+      navbar.addClass('awake');	
+    }
+    
+    if(sd.length > 0) {
+      sd.addClass('sleep');
+    }
   }
+  if ( st < 350 ) {
+    if ( navbar.hasClass('awake') ) {
+      navbar.removeClass('awake');
+      navbar.addClass('sleep');
+    }
+    if(sd.length > 0) {
+      sd.removeClass('sleep');
+    }
+  }
+});
+  }
+
 
 
  
