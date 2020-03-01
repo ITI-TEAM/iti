9cc6c13e4d92e2f12260923a8165c7a60a20b4bb
unknown <amlfakhry13@gmail.com>
2020-03-02 00:02:44 +0200

    saved job

--- server/models/savedJob.js
+++ server/models/savedJob.js
@@ -3,11 +3,10 @@ const mongoose = require('mongoose');
 const SavedJobSchema = new mongoose.Schema({
 
  // jobs:[{type: mongoose.Schema.Types.ObjectId , ref: 'allJob' }],
-  jobID:String,
+  jobID:[{type: mongoose.Schema.Types.ObjectId , ref: 'allJob' }],
   studentID:String
   
 });
 
 const SavedJob = mongoose.model('SavedJob', SavedJobSchema);
 
-module.exports = SavedJob;
--- server/routes/savedJob.js
+++ server/routes/savedJob.js
@@ -9,23 +9,39 @@ const SavedJob = require('../models/savedJob');
 
 
 router.post("/save/:jobID/:studentID", (req, res, next)=>{
-  const newSaved=new SavedJob({
-      jobID:req.params.jobID,
-      studentID:req.params.studentID
-  });
-  newSaved.save()
-  .then(result => {
-    console.log(result);
-    return res.status(200).json({result})
-  })
-  .catch(err => {
-    console.log(err);
-    res.status(501).json({
-      error: err
+  allJob.find({jobID:req.params.jobID})
+  .populate('jobID')
+  .exec()
+  .then(job => {
+    if (job.length >= 1) {
+      return res.status(409).json({
+        message: 'job already saved'
+      });
+    }if (!job) {
+      return res.status(404).json({
+        message: "job not found"
+      });
+    }
+    else{
+      const newSaved=new SavedJob({
+        jobID:req.params.jobID,
+        studentID:req.params.studentID
     });
-  });
+    newSaved.save()
+    .then(result => {
+      console.log(result);
+      return res.status(200).json({result})
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(501).json({
+        error: err
+      });
+    });
+    }
+ 
 });
-
+});
 
 
 router.get("/list/:studentID", (req, res, next)=>{
@@ -44,12 +60,6 @@ router.get("/list/:studentID", (req, res, next)=>{
     });
 
 });
-
-
-
-
-
-
 
 
 
